85032bdf7dfddaaed82b5dd39d3edb16e9aa84df
GrzegorzS87 <sobol.grz@gmail.com>
2023-01-29 22:45:47 +0100

    client data loading
    prepared client data in data xls and method for loading

--- src/test/java/utils/DataStorage.java
+++ src/test/java/utils/DataStorage.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Random;
 
 public class DataStorage {
 
@@ -46,6 +47,28 @@ public class DataStorage {
         }
 
         return productNames;
+    }
+
+    public Object[][] exampleClients(int max) {
+        Sheet sheet = workbook.getSheetAt(2);
+
+        int rowCount = sheet.getLastRowNum()+1;
+        if( rowCount > max ) rowCount = max;
+
+        Object[][] clients = new Object[rowCount][8];
+
+        for(int i=0; i<rowCount; i++){
+            clients[i][0] = sheet.getRow(i).getCell(0).getStringCellValue();
+            clients[i][1] = sheet.getRow(i).getCell(1).getStringCellValue();
+            clients[i][2] = sheet.getRow(i).getCell(2).getStringCellValue();
+            clients[i][3] = sheet.getRow(i).getCell(3).getStringCellValue();
+            clients[i][4] = sheet.getRow(i).getCell(4).getBooleanCellValue();
+            clients[i][5] = sheet.getRow(i).getCell(5).getStringCellValue();
+            clients[i][6] = sheet.getRow(i).getCell(6).getStringCellValue();
+            clients[i][7] = sheet.getRow(i).getCell(7).getStringCellValue();
+        }
+
+        return clients;
     }
 
 }
